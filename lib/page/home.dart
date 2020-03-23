8e67a13cccfc28a96a84b3448bbe724814b62101
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-23 19:53:05 +0300

    Create error handler widget

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 import 'package:coronavirus/api/client.dart';
 import 'package:coronavirus/model/Api.dart';
-import 'package:flutter/material.dart';
+import 'package:coronavirus/model/Country.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -32,88 +35,14 @@ class _HomePageState extends State<HomePage> {
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               if (snapshot.hasError) {
-                return Center(
-                  child: Text("Oops someting went't wrong"),
+                return SomethingWentWrong(
+                  errorMessage: snapshot.error.toString(),
                 );
               }
               return ListView.builder(
                   itemCount: snapshot.data.countries.length,
                   itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 8.0,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                      child: Container(
-                        child: ListTile(
-                            contentPadding: EdgeInsets.fromLTRB(16, 2, 10, 0),
-                            title: Text(
-                              '${snapshot.data.countries[index].name}',
-                              style: TextStyle(fontSize: 24),
-                              textAlign: TextAlign.center,
-                            ),
-                            subtitle: Container(
-                                padding: EdgeInsets.fromLTRB(0, 25, 0, 16),
-                                child: Column(
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Column(
-                                          children: <Widget>[
-                                            Text('Total Cases'),
-                                            Text(
-                                                '${snapshot.data.countries[index].totalCases}')
-                                          ],
-                                        ),
-                                        Column(
-                                          children: <Widget>[
-                                            Text('New Cases'),
-                                            Text(
-                                                '${snapshot.data.countries[index].newCases}')
-                                          ],
-                                        ),
-                                        Column(
-                                          children: <Widget>[
-                                            Text('Total Deaths'),
-                                            Text(
-                                                '${snapshot.data.countries[index].totalDeaths}')
-                                          ],
-                                        ),
-                                        Column(
-                                          children: <Widget>[
-                                            Text('New Deaths'),
-                                            Text(
-                                                '${snapshot.data.countries[index].newDeaths}')
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.only(top: 15),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                        children: <Widget>[
-                                          Column(
-                                            children: <Widget>[
-                                              Text('Total Recovered'),
-                                              Text(
-                                                  '${snapshot.data.countries[index].totalRecovered}')
-                                            ],
-                                          ),
-                                          Column(
-                                            children: <Widget>[
-                                              Text('Active Cases'),
-                                              Text(
-                                                  '${snapshot.data.countries[index].activeCases}')
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ))),
-                      ),
-                    );
+                    return _countryCard(snapshot.data.countries, index);
                   });
             } else {
               return CircularProgressIndicator();
@@ -123,4 +52,104 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  final String errorMessage;
+
+  SomethingWentWrong({this.errorMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('graphics/error-404.png'),
+                width: 250,
+                height: 250,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "Oops Something Went't Wrong",
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 32.0),
+                  child: Text(
+                    errorMessage,
+                    style: TextStyle(color: Colors.black87, fontSize: 15.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 250.0,
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () async {
+                    await SystemNavigator.pop();
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(45.0),
+                  ),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  child: Text(
+                    "Try Again",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+_countryCard(List<Country> countiries, int index) {
+  return Card(
+    elevation: 4.0,
+    margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+    child: _countryListItem(countiries[index].name),
+  );
+}
+
+_countryListItem(String countryName) {
+  return ListTile(
+    contentPadding: EdgeInsets.fromLTRB(16, 2, 10, 0),
+    title: Text(
+      countryName,
+      style: TextStyle(fontSize: 24),
+      textAlign: TextAlign.center,
+    ),
+  );
 }
