07a5e39f79198ba73e06d92f833b77df799efba8
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-24 01:02:50 +0300

    Source code refactored

--- lib/model/Api.dart
+++ lib/model/Api.dart
@@ -8,7 +8,6 @@ class Api {
 
   factory Api.fromJson(Map<String, dynamic> json) {
     var list = json['result'] as List;
-    print(list.runtimeType);
     List<Country> countryList = list.map((c) => Country.fromJson(c)).toList();
 
     return Api(success: json['success'], countries: countryList);
