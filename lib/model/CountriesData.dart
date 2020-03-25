1213ae27a7a1e29e269dd039c99d40be3a22c933
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-25 04:10:48 +0300

    Changed class name to better one

--- lib/model/CountriesData.dart
+++ lib/model/CountriesData.dart
@@ -1,15 +1,15 @@
 import 'package:coronavirus/model/Country.dart';
 
-class Api {
+class CountriesData {
   final bool success;
   final List<Country> countries;
 
-  Api({this.success, this.countries});
+  CountriesData({this.success, this.countries});
 
-  factory Api.fromJson(Map<String, dynamic> json) {
+  factory CountriesData.fromJson(Map<String, dynamic> json) {
     var list = json['result'] as List;
     List<Country> countryList = list.map((c) => Country.fromJson(c)).toList();
 
-    return Api(success: json['success'], countries: countryList);
+    return CountriesData(success: json['success'], countries: countryList);
   }
 }
