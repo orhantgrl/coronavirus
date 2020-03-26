d12f414222d7d3c6de3dc7ba337b887517d75e31
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-26 21:23:21 +0300

    Add new function for fetch total data

--- lib/api/client.dart
+++ lib/api/client.dart
@@ -1,26 +1,42 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:coronavirus/model/Api.dart';
+import 'package:coronavirus/model/CountriesData.dart';
+import 'package:coronavirus/model/TotalData.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class WebService {
-  final String url = 'https://api.collectapi.com/corona/countriesData';
+  final String countriesDataUrl = 'https://api.collectapi.com/corona/countriesData';
+  final String totalDataUrl = 'https://api.collectapi.com/corona/totalData';
   String token;
 
-  Future<Api> fetchCountries() async {
-    final response = await http.get(url, headers: {
-      HttpHeaders.contentTypeHeader: 'application/json',
-      HttpHeaders.authorizationHeader: token
-    });
+  Future<CountriesData> fetchCountriesData() async {
+    final response = await request(url: countriesDataUrl, token: token);
 
     if (response.statusCode == 200) {
-      return Api.fromJson(jsonDecode(response.body));
+      return CountriesData.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Client error');
+      throw Exception('Api client error check with debug option');
     }
   }
+
+  Future<TotalData> fetchTotalData() async {
+    final response = await request(url: totalDataUrl, token: token);
+
+    if(response.statusCode == 200) {
+      return TotalData.fromJson(jsonDecode(response.body)['result']);
+    } else {
+      throw Exception('Api client error check with debug option');
+    }
+  }
+}
+
+Future<http.Response> request({String url, String token}) async {
+  return await http.get(url, headers: {
+    HttpHeaders.contentTypeHeader: 'application/json',
+    HttpHeaders.authorizationHeader: token
+  });
 }
 
 Future<String> getApplicationToken() async {
