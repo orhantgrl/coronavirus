4c9d4822a21e1ecd6a29e7b675a5141185b63564
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-26 21:24:37 +0300

    Add test for total data fetching

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,15 +1,29 @@
 import 'package:coronavirus/api/client.dart';
-import 'package:coronavirus/model/Api.dart';
+import 'package:coronavirus/model/CountriesData.dart';
+import 'package:coronavirus/model/TotalData.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Test for api response', () async {
+  test('Test for countries data', () async {
     var client = WebService();
     String token = ""; // Your secret api token :)
 
     client.token = token;
-    await client.fetchCountries().then((Api api) {
+    await client.fetchCountriesData().then((CountriesData api) {
       expect('China', api.countries[0].name); // Passed
+    });
+  });
+
+  test('Test for total data', () async {
+    var client = WebService();
+    String token = ""; // Your secret api token :)
+
+    client.token = token;
+    await client.fetchTotalData().then((TotalData data) {
+      /* Check current death of number here: 
+       * https://www.worldometers.info/coronavirus/coronavirus-death-toll/
+       */
+      expect('17,138', data.totalDeaths); // Passed
     });
   });
 }
