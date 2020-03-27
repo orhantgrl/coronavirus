29be8b9a566b4fd17828252c82c0a07d386ef3d6
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-27 19:52:26 +0300

    Source code refactored

--- lib/model/TotalData.dart
+++ lib/model/TotalData.dart
@@ -3,13 +3,13 @@ class TotalData {
   String totalCases;
   String totalRecovered;
 
-  TotalData({this.totalDeaths, this.totalCases,  this.totalRecovered});
+  TotalData({this.totalDeaths, this.totalCases, this.totalRecovered});
 
   factory TotalData.fromJson(Map<String, dynamic> json) {
     return TotalData(
       totalDeaths: json['totalDeaths'],
       totalCases: json['totalCases'],
-      totalRecovered: json['totalRecovered']
+      totalRecovered: json['totalRecovered'],
     );
   }
 }
