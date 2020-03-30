84e0c1bac284612ae3157271d97fcecfb4f180ae
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-30 12:07:43 +0300

    Source code refactored but need more maintenance

--- lib/widget/root.dart
+++ lib/widget/root.dart
@@ -1,8 +1,9 @@
-import 'package:coronavirus/api/data.dart';
 import 'package:coronavirus/page/aboutme.dart';
 import 'package:coronavirus/page/home.dart';
 import 'package:coronavirus/page/stats.dart';
 import 'package:flutter/material.dart';
+
+import '../page/stats.dart';
 
 class WidgetRoot extends StatefulWidget {
   WidgetRoot({Key key}) : super(key: key);
@@ -12,22 +13,8 @@ class WidgetRoot extends StatefulWidget {
 }
 
 class _WidgetRootState extends State<WidgetRoot> {
-  Future<CountriesWithTotalData> _countriesWithTotalData;
-
   int _selectedIndex = 0;
   String _selectedTitle = "#StayAtHomeSaveLives";
-
-  @override
-  void initState() {
-    super.initState();
-    _countriesWithTotalData = startTransaction();
-  }
-
-  void _callback() {
-    setState(() {
-      _countriesWithTotalData = startTransaction();
-    });
-  }
 
   final List<String> _titles = <String>[
     '#StayAtHomeSaveLives',
@@ -44,48 +31,15 @@ class _WidgetRootState extends State<WidgetRoot> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<CountriesWithTotalData>(
-      future: _countriesWithTotalData,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If application didn't load data from service update splash widget
-          if (snapshot.hasError) {
-            return Scaffold(
-              body: _splash(
-                "Oops Something Wen't Wrong",
-                snapshot.error.toString(),
-                true,
-                callback: _callback,
-              ),
-            );
-          }
-
-          // If application load data successfully show pages
-          return _mainFrame(
-            HomePage(
-              countries: snapshot.data.countriesData.countries,
-            ),
-            StatsPage(
-              totalData: snapshot.data.totalData,
-              countries: snapshot.data.countriesData.countries,
-            ),
-          );
-
-          // If process not done running show splash screen
-        } else {
-          return Scaffold(
-            body: _splash("Welcome The Corona Virus Tracker",
-                "Make sure internet connection is active", false),
-          );
-        }
-      },
+    return Scaffold(
+      body:_initApp()
     );
   }
 
-  Widget _mainFrame(HomePage _homePage, StatsPage _statsPage) {
+  Widget _initApp() {
     final List<Widget> _pages = <Widget>[
-      _homePage,
-      _statsPage,
+      HomePage(),
+      StatsPage(),
       AboutMePage(),
     ];
 
@@ -119,92 +73,3 @@ class _WidgetRootState extends State<WidgetRoot> {
     );
   }
 }
-
-Widget _splash(String _headerText, String _paragraph, bool _isFailure,
-        {Function callback}) =>
-    Center(
-      child: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.fromLTRB(32.0, 0.0, 32.0, 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image(
-                  image: AssetImage('graphics/hiker.png'),
-                  width: 250,
-                  height: 250,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      _headerText,
-                      style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 32.0),
-                    child: Text(
-                      _paragraph,
-                      style: TextStyle(color: Colors.black87, fontSize: 15.0),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Visibility(
-                      visible: !_isFailure,
-                      child: CircularProgressIndicator(),
-                    ),
-                    Visibility(
-                      visible: _isFailure,
-                      child: Container(
-                        width: 250.0,
-                        height: 50.0,
-                        child: RaisedButton(
-                          onPressed: () {
-                            callback();
-                          },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(45.0),
-                          ),
-                          color: Colors.black,
-                          textColor: Colors.white,
-                          child: Text(
-                            "Try Again",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
