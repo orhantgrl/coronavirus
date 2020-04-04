5ca168b62575abd5edb46ae5d46f98fadd262bd7
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-04-04 18:53:41 +0300

    CountriesData class combined with Country class

--- lib/model/CountriesData.dart
+++ lib/model/CountriesData.dart
@@ -1,5 +1,3 @@
-import 'package:coronavirus/model/Country.dart';
-
 class CountriesData {
   final bool success;
   final List<Country> countries;
@@ -11,5 +9,35 @@ class CountriesData {
     List<Country> countryList = list.map((c) => Country.fromJson(c)).toList();
 
     return CountriesData(success: json['success'], countries: countryList);
+  }
+}
+
+class Country {
+  final String name;
+  final String totalCases;
+  final String newCases;
+  final String totalDeaths;
+  final String newDeaths;
+  final String totalRecovered;
+  final String activeCases;
+
+  Country(
+      {this.name,
+      this.totalCases,
+      this.newCases,
+      this.totalDeaths,
+      this.newDeaths,
+      this.totalRecovered,
+      this.activeCases});
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+        name: json['country'],
+        totalCases: json['totalCases'],
+        newCases: json['newCases'],
+        totalDeaths: json['totalDeaths'],
+        newDeaths: json['newDeaths'],
+        totalRecovered: json['totalRecovered'],
+        activeCases: json['activeCases']);
   }
 }
