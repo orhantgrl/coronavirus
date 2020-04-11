f3dad97225c7caac5ee8e8d061f986cf21a3c226
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-04-12 02:03:31 +0300

    Source code refactored for security

--- lib/resource/api_provider.dart
+++ lib/resource/api_provider.dart
@@ -13,7 +13,7 @@ class ApiProvider {
   final String _totalDataUrl = 'https://api.collectapi.com/corona/totalData';
 
   Future<CountriesData> fetchCountriesData() async {
-    final response = await request(url: _countriesDataUrl);
+    final response = await _request(url: _countriesDataUrl);
 
     if (response.statusCode == 200) {
       return CountriesData.fromJson(jsonDecode(response.body));
@@ -23,7 +23,7 @@ class ApiProvider {
   }
 
   Future<TotalData> fetchTotalData() async {
-    final response = await request(url: _totalDataUrl);
+    final response = await _request(url: _totalDataUrl);
 
     if (response.statusCode == 200) {
       return TotalData.fromJson(jsonDecode(response.body)['result']);
@@ -31,18 +31,18 @@ class ApiProvider {
       throw Exception('Failed to load total data');
     }
   }
-}
 
-request({@required String url}) async {
-  Client client = Client();
-  return await client.get(url, headers: {
-    HttpHeaders.contentTypeHeader: 'application/json',
-    HttpHeaders.authorizationHeader: await getApplicationToken()
-  });
-}
+  _request({@required String url}) async {
+    Client client = Client();
+    return await client.get(url, headers: {
+      HttpHeaders.contentTypeHeader: 'application/json',
+      HttpHeaders.authorizationHeader: await _getApplicationToken()
+    });
+  }
 
-Future<String> getApplicationToken() async {
-  final parsedJson =
-      jsonDecode(await rootBundle.loadString('assets/config.json'));
-  return parsedJson['token'];
+  Future<String> _getApplicationToken() async {
+    final parsedJson =
+    jsonDecode(await rootBundle.loadString('assets/config.json'));
+    return parsedJson['token'];
+  }
 }
